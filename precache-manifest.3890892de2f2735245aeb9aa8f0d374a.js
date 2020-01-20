496326a807eddd265361d79f7e6777a6658cb94d
Courtney <dowelc@cs.washington.edu>
2020-01-20 14:43:38 -0800

    Updates

---
+++ precache-manifest.3890892de2f2735245aeb9aa8f0d374a.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a607195367b843a8febac4711198803e",
+    "url": "/rpsls/index.html"
+  },
+  {
+    "revision": "5bd390abdf0bfadbfb47",
+    "url": "/rpsls/static/js/2.ebeba8e8.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/rpsls/static/js/2.ebeba8e8.chunk.js.LICENSE"
+  },
+  {
+    "revision": "54f0e73443c3c950b5c2",
+    "url": "/rpsls/static/js/main.fc9dd697.chunk.js"
+  },
+  {
+    "revision": "de02b07cecba272450c3",
+    "url": "/rpsls/static/js/runtime-main.fa4752b5.js"
+  },
+  {
+    "revision": "2a42d61ebe96dacb620aae0401e88179",
+    "url": "/rpsls/static/media/lizard.2a42d61e.jpg"
+  },
+  {
+    "revision": "7c607c3debdd5132a7feed0489e0f557",
+    "url": "/rpsls/static/media/paper.7c607c3d.png"
+  },
+  {
+    "revision": "ff826bd11d7fdaf0afad14b1ebff9e81",
+    "url": "/rpsls/static/media/rock.ff826bd1.png"
+  },
+  {
+    "revision": "73c012ea18118611bbb7774c018a7556",
+    "url": "/rpsls/static/media/scissors.73c012ea.jpg"
+  },
+  {
+    "revision": "524f1a73a54e53cabd8a62cb0b4fd2f3",
+    "url": "/rpsls/static/media/spock.524f1a73.png"
+  }
+]);
