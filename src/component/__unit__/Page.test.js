0a8de347f2e1874be1439fac2d0294c3cd5bedc2
Courtney <dowelc@cs.washington.edu>
2020-01-20 23:07:22 -0800

    Update remaining tests

--- src/component/__unit__/Page.test.js
+++ src/component/__unit__/Page.test.js
@@ -2,6 +2,11 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import Page from '../Page';
 
+jest.mock('../Credits', () => 'Credits');
+jest.mock('../Score', () => 'Score');
+jest.mock('../PlayArea', () => 'PlayArea');
+jest.mock('../UrlPicker', () => 'UrlPicker');
+
 test('Page renders successfully', () => {
     const { getByText } = render(<Page />);
     expect(getByText(/Rock Paper Scissors Lizard Spock!/)).toBeInTheDocument();
