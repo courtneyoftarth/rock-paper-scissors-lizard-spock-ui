d54f56d796cffae2952dbf02a603b999bc7ed560
Courtney <dowelc@cs.washington.edu>
2020-01-20 22:55:28 -0800

    Add all credits

--- src/component/Credits.js
+++ src/component/Credits.js
@@ -1,12 +1,21 @@
 import React from 'react';
 
+const sources = {
+    lizard: 'https://www.istockphoto.com/vector/friendly-green-iguana-in-flat-style-vector-gm862153016-142888289',
+    paper: 'https://www.pinclipart.com/pindetail/Txmmx_counseling-clipart-student-meeting-paper-black-and-white/',
+    rock: 'https://clipartart.com/categories/rock-clipart-free.html',
+    scissors: 'http://www.clipartpanda.com/clipart_images/scissors-clipart-5181114',
+    spock: 'https://www.pinclipart.com/pindetail/ohwhox_star-trek-spock-fallout-star-trek-spock-fallout/'
+};
+
 const Credits = () => (
     <div>
         <h2>Credits</h2>
         Pictures taken from
         <ul>
-            <li><a href="https://www.istockphoto.com/vector/friendly-green-iguana-in-flat-style-vector-gm862153016-142888289">Lizard</a></li>
-            <li><a href="https://www.pinclipart.com/pindetail/ohwhox_star-trek-spock-fallout-star-trek-spock-fallout/">Spock</a></li>
+            {
+                Object.keys(sources).map(move => <li><a href={sources[move]}>{move}</a></li>)
+            }
         </ul>
     </div>
 );
